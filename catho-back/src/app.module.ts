fc95c84d2baf28be16ab75be298ea4371e21a397
Abner <abner.rodrigues@zooxsmart.com>
2024-02-01 09:17:31 -0300

    adiciona rateLimiter de 10 requisições por minuto para evitar SPAM.

--- catho-back/src/app.module.ts
+++ catho-back/src/app.module.ts
@@ -2,17 +2,30 @@ import { Module } from '@nestjs/common';
 import { CandidatesModule } from './candidates/candidates.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
     MongooseModule.forRoot(process.env.MONGODB_URI, {
       dbName: 'catho',
     }),
     CandidatesModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- catho-back/src/candidates/candidates.controller.ts
+++ catho-back/src/candidates/candidates.controller.ts
@@ -10,26 +10,30 @@ import {
 import { CandidatesService } from './candidates.service';
 import { CreateCandidateDto } from './dto/create-candidate.dto';
 import { SearchSkillCandidateDto } from './dto/search-skill-candidate.dto';
+import { Throttle } from '@nestjs/throttler';
 
 @Controller('candidates')
 export class CandidatesController {
   constructor(private readonly candidatesService: CandidatesService) {}
 
   @Post()
+  @Throttle({ default: { limit: 10, ttl: 60000 } })
   @UsePipes(new ValidationPipe({ transform: true }))
-  create(@Body() createCandidateDto: CreateCandidateDto) {
+  async create(@Body() createCandidateDto: CreateCandidateDto) {
     return this.candidatesService.create(createCandidateDto);
   }
 
   @Get('search')
+  @Throttle({ default: { limit: 10, ttl: 60000 } })
   @UsePipes(new ValidationPipe({ transform: true }))
-  findBySkills(@Query() query: SearchSkillCandidateDto) {
+  async findBySkills(@Query() query: SearchSkillCandidateDto) {
     return this.candidatesService.findBySkills(query);
   }
 
   @Get()
+  @Throttle({ default: { limit: 10, ttl: 60000 } })
   @UsePipes(new ValidationPipe({ transform: true }))
-  findAll() {
+  async findAll() {
     return this.candidatesService.findAll();
   }
 }
